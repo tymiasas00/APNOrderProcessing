e39a613d6fbcf65b649b283454db5dc1dd7f78e1
Tymoteusz Majewski <tymoteusz.majewski1@gmail.com>
2025-03-14 14:32:42 +0100

    Added incorrect values handling and adjusted status enum.

--- APNOrderProcessing/Models/Order.cs
+++ APNOrderProcessing/Models/Order.cs
@@ -22,19 +22,56 @@ namespace APNOrderProcessing.Models
         New,
         InWarehouse,
         InDelivery,
-        Delivered,
+        ReturnedToClient,
         Error,
         Closed
     }
     public class Order
     {
-        public string ProductName { get; set; } = string.Empty;
+        public string ProductName { get; set; }
         public decimal OrderPrice { get; set; }
         public TypeOfClient ClientType { get; set; }
 
-        public string Address { get; set; } = string.Empty;
+        public string? Address { get; set; } = string.Empty;
         public TypeOfPayment PaymentType { get; set; }
 
+        public Status OrderStatus { get; set; }
 
-    }
-}
+        public Order(
+            string productName,
+            decimal orderPrice,
+            TypeOfClient clientType,
+            string address,
+            TypeOfPayment paymentType,
+            Status orderStatus = Status.New)
+        {
+            if (string.IsNullOrWhiteSpace(productName))
+                throw new ArgumentNullException(
+                    nameof(productName),
+                    "Product name cannot be empty.");
+
+            if (orderPrice <= 0)
+                throw new ArgumentException(
+                    "Incorrect order price.",
+                    nameof(orderPrice));
+
+            if (string.IsNullOrWhiteSpace(address))
+                throw new ArgumentNullException(
+                    nameof(address),
+                    "Address cannot be empty.");
+
+            ProductName = productName;
+            OrderPrice = orderPrice;
+            ClientType = clientType;
+            Address = address;
+            PaymentType = paymentType;
+            OrderStatus = orderStatus;
+
+            if (PaymentType == TypeOfPayment.Cash && orderPrice >= 2500)
+                OrderStatus = Status.ReturnedToClient;
+
+
+
+
+        }
+    } }
